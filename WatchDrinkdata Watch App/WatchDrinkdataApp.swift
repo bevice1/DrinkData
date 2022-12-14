8d4042e13d380e515ecff9431b37dc3b1dda258b
Mayr08 <mayr97.sm@gmx.at>
2022-12-14 15:34:38 +0100

    Watch OS App created

---
+++ WatchDrinkdata Watch App/WatchDrinkdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchDrinkdataApp.swift
+//  WatchDrinkdata Watch App
+//
+//  Created by Simon Mayr on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct WatchDrinkdata_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
